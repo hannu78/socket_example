7603568f9b21b899ecf2ccb9f81fbda63986d4f9
hannu78 <hannu.lisko@iki.fi>
2016-01-20 14:55:19 +0200

    luotu chattiapplikaatio keskiviikko 20012016

---
+++ chat_scripts.js
@@ -0,0 +1,24 @@
+window.onload = function (event) {
+    console.log("Chat ready");
+    //Connect to server with socket
+    var socket = io();
+    //Get "send" object
+    var btnSend = document.getElementById('send');
+    //Add click listener
+    btnSend.onclick = function () {
+        var msg = document.getElementById('chat_message');
+        console.log(msg.value);
+        //msg.value or an object can be sent to server
+        var dataToServer = {
+            name:'undefined',
+            message: msg.value
+        }
+        //Send object to server
+        socket.emit('chat msg', dataToServer);
+        msg.value = "";
+    }
+    socket.on('new message', function (data) {
+        var textArea = document.getElementById('messages');
+        textArea.value += data.name + ": " + data.message + "\n";
+    })
+}
